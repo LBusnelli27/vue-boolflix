b65e9fad5a8ad7bc9653fe7fa689fee5ae867dc6
Busna27 <lucabusnelli2001@gmail.com>
2022-04-01 14:44:05 +0200

    set up + added bootstrap & axios

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,8 @@
 </template>
 
 <script>
+import 'bootstrap/dist/css/bootstrap.css';
+
 import Main from './components/Main.vue'
 
 export default {
@@ -16,12 +18,5 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,15 +1,31 @@
 <template>
   <div>
-    
+    <div class="row">
+      <div class="col-12 d-flex p-5">
+        <input class="me-2" type="text">
+        <button class="btn btn-primary">Search</button>
+      </div>
+      <div class="col-12">
+        <ul></ul>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name : 'MainComponent',
+
+  mounted () {
+    axios
+      .get('https://api.themoviedb.org/3/movie/550?api_key=8e367e7bbe971330f3e274f6b4bddf1e')
+      .then(response => (this.albumList = response.data.response));
+  }
 }
 </script>
 
 <style>
-
+  
 </style>
