a215c488160fa97ea951a1085b0604bc0a8d8383
Busna27 <lucabusnelli2001@gmail.com>
2022-04-01 15:19:09 +0200

    search and show films results

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,11 +2,40 @@
   <div>
     <div class="row">
       <div class="col-12 d-flex p-5">
-        <input class="me-2" type="text">
-        <button class="btn btn-primary">Search</button>
+        <input v-model="inputSearch" @keyup.enter="searchApiTheMovie()" class="me-2" type="text">
+        <button @click="searchApiTheMovie()" class="btn btn-primary">Search</button>
       </div>
-      <div class="col-12">
-        <ul></ul>
+      <div class="col-3 px-4">
+        <h3>Title</h3>
+        <ul>
+          <li class="mb-2" v-for="(element, index) in listOfSearched" :key="index">
+            {{ element.title }}
+          </li>
+        </ul>
+      </div>
+      <div class="col-3 px-4">
+        <h3>Original Title</h3>
+        <ul>
+          <li class="mb-2" v-for="(element, index) in listOfSearched" :key="index">
+            {{ element.original_title }}
+          </li>
+        </ul>
+      </div>
+      <div class="col-3 px-4">
+        <h3>Language</h3>
+        <ul>
+          <li class="mb-2" v-for="(element, index) in listOfSearched" :key="index">
+            {{ element.original_language }}
+          </li>
+        </ul>
+      </div>
+      <div class="col-3 px-4">
+        <h3>Vote</h3>
+        <ul>
+          <li class="mb-2" v-for="(element, index) in listOfSearched" :key="index">
+            {{ element.vote_average }}
+          </li>
+        </ul>
       </div>
     </div>
   </div>
@@ -18,10 +47,19 @@ import axios from 'axios';
 export default {
   name : 'MainComponent',
 
-  mounted () {
-    axios
-      .get('https://api.themoviedb.org/3/movie/550?api_key=8e367e7bbe971330f3e274f6b4bddf1e')
-      .then(response => (this.albumList = response.data.response));
+  data() {
+    return {
+      inputSearch : '',
+      listOfSearched : []
+    }
+  },
+
+  methods: {
+    searchApiTheMovie() {
+      axios
+      .get('https://api.themoviedb.org/3/search/movie?api_key=8e367e7bbe971330f3e274f6b4bddf1e&query=' + this.inputSearch.replace(/\s/g , "+"))
+      .then(response => (this.listOfSearched = response.data.results));
+    }
   }
 }
 </script>
